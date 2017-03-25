3451d528451b344dfd0f5ae3c934c0ec7e8ad4a1
sankulka <santoshnkulkarni@gmail.com>
2017-03-25 17:26:06 +0530

    New config

--- config/auth.js
+++ config/auth.js
@@ -17,7 +17,7 @@ var exports = module.exports = 	{
 };
 
 var client_id = '1077116939953-cbrkk3fd5o7obr27f644r5qgtgngqssb.apps.googleusercontent.com';
-var client_secret = 'j0U7o-rdwryuEVqAyyAjzQa5';
+var client_secret = 'OaVTvYj3ETSKzdas4bDX-gs_';
 var redirect_uris = 'http://nuhomoeo-node-nuhomoeo.44fs.preview.openshiftapps.com/oauth2callback';
 
 exports.initial = 'SNKK';
